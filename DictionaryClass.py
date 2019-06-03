331d9028336411c2c6ad138ce2aa1d109640a3b9
Armaan Hossain <43430610+ArmaanJH@users.noreply.github.com>
2019-06-03 12:47:32 -0400

    Create DictionaryClass.py

---
+++ DictionaryClass.py
@@ -0,0 +1,12 @@
+class Dict:
+    def __init__(self):
+        self.key = ""
+        self.value = 0
+
+    def KeyandVal(self):
+        
+    def ChangeVal(self, key):
+
+    def SwapVal(self, key1, key2):
+
+    def display(self, dict):
